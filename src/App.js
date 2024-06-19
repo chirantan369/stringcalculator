c7180d21e3ea7159fad96eed7e1f8b97f859ab74
Chirantan Patel <chirantan369@gmail.com>
2024-06-19 19:01:38 +0530

    input added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [numberWithString, setNumberWithString] = useState("");
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        String Calculator
+        <div>
+          <input type='text' value={numberWithString} placeholder='Enter Number' onChange={(e) => { setNumberWithString(e.target.value) }} />
+        </div>
       </header>
+
     </div>
   );
 }
